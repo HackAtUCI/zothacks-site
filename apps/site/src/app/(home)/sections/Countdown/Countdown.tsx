5cb316e9c2ea81246add340ca54f184682406cb3
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-09-30 16:29:33 -0700

    formatting and style alignment

--- apps/site/src/app/(home)/sections/Countdown/Countdown.tsx
+++ apps/site/src/app/(home)/sections/Countdown/Countdown.tsx
@@ -22,9 +22,9 @@ interface CountdownProps {
 }
 
 const Countdown: React.FC<CountdownProps> = ({ schedule }) => {
-	const curTime = new Date("2023-11-04T02:00:00.000Z");
+	const curTime = new Date("2023-11-04T01:30:00.000Z");
 	const endTime = new Date("2024-11-04T02:00:00.000Z");
-	console.log(schedule);
+
 	const ended = schedule.filter((el) => el.endTime > curTime);
 
 	console.log(ended);
@@ -49,6 +49,12 @@ const Countdown: React.FC<CountdownProps> = ({ schedule }) => {
 					endTime: new Date(0),
 				};
 
+	const percentageCrossed =
+		before.endTime.getTime() > 0
+			? (before.endTime.getTime() - curTime.getTime()) /
+				(before.endTime.getTime() - before.startTime.getTime())
+			: 100;
+
 	const [w, h] = useWindow();
 
 	const totalLines = Math.floor(w / 66);
@@ -72,63 +78,68 @@ const Countdown: React.FC<CountdownProps> = ({ schedule }) => {
 	return (
 		<div className={styles.countdownWrapper}>
 			<Image src={bg_map} alt="bg_map" />
-			<div className={styles.countdownMaterial}>
-				<div className={styles.progressContainer}>
-					<div
-						style={{ width: `${Math.floor(w / 1.7)}px`, height: `100px` }}
-						className={styles.paraboloid}
-					>
-						{totals.map((el, i) => (
+			{w > 0 && (
+				<div className={styles.countdownMaterial}>
+					<div className={styles.progressContainer}>
+						<div
+							style={{ width: `${Math.floor(w / 1.7)}px`, height: `100px` }}
+							className={styles.paraboloid}
+						>
+							{totals.map((el, i) => (
+								<div
+									key={i}
+									style={{
+										left: returnPosition(i)[0],
+										bottom: returnPosition(i)[1],
+										position: "absolute",
+										backgroundColor: `${i / totals.length > percentageCrossed ? "#DB9F42" : "#78cae3"}`,
+										width: "18px",
+										height: "5px",
+										borderRadius: "6px",
+										transform: `rotate(${-((returnRotation(i) * 180) / Math.PI)}deg)`,
+									}}
+								></div>
+							))}
 							<div
-								key={i}
+								className={styles.outerCircle}
 								style={{
-									left: returnPosition(i)[0],
-									bottom: returnPosition(i)[1],
-									position: "absolute",
-									backgroundColor: "#DB9F42",
-									width: "18px",
-									height: "5px",
-									borderRadius: "6px",
-									transform: `rotate(${-((returnRotation(i) * 180) / Math.PI)}deg)`,
+									bottom: "-20px",
+									left: "-20px",
 								}}
-							></div>
-						))}
-						<div
-							className={styles.outerCircle}
-							style={{
-								bottom: "-20px",
-								left: "-20px",
-							}}
-						>
-							<div className={styles.innerCircle}></div>
+							>
+								<div className={styles.innerCircle}></div>
+								<div className={styles.descriptiveText}>
+									<h5>{before.title}</h5>
+									{before?.location && <p>{before.location}</p>}
+									{before?.startTime.getTime() ? (
+										<p>{`${before.startTime.getHours() % 12} ${before.startTime.getHours() == 11 ? "am" : ""}-${before.endTime.getHours() % 12} ${before.endTime.getHours() < 12 ? "am" : "pm"}`}</p>
+									) : null}
+								</div>
+							</div>
+
+							<div
+								className={styles.outerCircle}
+								style={{
+									bottom: "-20px",
+									right: "-38px",
+								}}
+							>
+								<div className={styles.innerCircle}></div>
+								<div className={styles.descriptiveText}>
+									<h5>{after.title}</h5>
+									{after.location && <p>{after.location}</p>}
+									{after.startTime.getTime() ? (
+										<p>{`${after.startTime.getHours() % 12}${after.startTime.getHours() == 11 ? " am" : ""}-${after.endTime.getHours() % 12} ${after.endTime.getHours() < 12 ? "am" : "pm"}`}</p>
+									) : null}
+								</div>
+							</div>
 						</div>
 						<div>
-							<h5>{before.title}</h5>
-							{before?.location && <p>{before.location}</p>}
-							{before?.startTime.getTime() ? (
-								<p>{`${before.startTime.getHours() % 12} ${before.startTime.getHours() == 11 ? "am" : ""}-${before.endTime.getHours() % 12} ${before.endTime.getHours() < 12 ? "am" : "pm"}`}</p>
-							) : null}
-						</div>
-						<div
-							className={styles.outerCircle}
-							style={{
-								bottom: "-20px",
-								right: "-38px",
-							}}
-						>
-							<div className={styles.innerCircle}></div>
-						</div>
-						<div>
-							<h5>{after.title}</h5>
-							{after.location && <p>{after.location}</p>}
-							{after.startTime.getTime() ? (
-								<p>{`${after.startTime.getHours() % 12}${after.startTime.getHours() == 11 ? " am" : ""}-${after.endTime.getHours() % 12} ${after.endTime.getHours() < 12 ? "am" : "pm"}`}</p>
-							) : null}
+							<CountdownClock countdownTo={endTime} isHackingStarted={false} />
 						</div>
 					</div>
 				</div>
-				<CountdownClock countdownTo={endTime} isHackingStarted={false} />
-			</div>
+			)}
 		</div>
 	);
 };
