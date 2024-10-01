af6736753ceef5121fc2b03c6bf495debcfd8d99
Jennifer <jennil38@uci.edu>
2024-09-30 19:46:35 -0700

    first draft of footer - needs to change links

--- apps/site/src/components/Footer/Footer.tsx
+++ apps/site/src/components/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
 
 import styles from "./Footer.module.scss";
 
-import Mail from "@/assets/icons/email.svg";
-import Facebook from "@/assets/icons/Facebook-Logo-Primary-min.svg";
-import Instagram from "@/assets/icons/instagram.png";
-import Hack from "@/assets/icons/hack-logo.svg";
-import TikTok from "@/assets/icons/TikTok-Social-Icon-Circle-White.svg";
+
+import Mail from "@/assets/icons/mail_icon.svg";
+import Facebook from "@/assets/icons/facebook_icon.svg";
+import Instagram from "@/assets/icons/instagram_icon.svg";
+import Hack from "@/assets/icons/HACK LOGO.svg";
+import TikTok from "@/assets/icons/tiktok_icon.svg";
 
 type Social = {
 	icon: any;
@@ -16,12 +19,15 @@ type Social = {
 	alt: string;
 };
 
-const SOCIALS: Social[] = [
+const LEFT_SOCIALS: Social[] = [
 	{
 		icon: Hack,
 		link: "https://hack.ics.uci.edu/",
 		alt: "Hack at UCI logo that links to Hack at UCI's club website",
-	},
+	}
+];
+
+const RIGHT_SOCIALS: Social[] = [
 	{
 		icon: Mail,
 		link: "mailto:hack@uci.edu",
@@ -47,20 +53,63 @@ const SOCIALS: Social[] = [
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<p className={styles.text}>Made with &lt;3 in Irvine, CA - Hack @ UCI</p>
-			<div className="text-center">
-				{SOCIALS.map(({ icon, link, alt }) => (
-					<a key={link} href={link} target="_blank">
-						<Image
-							src={icon.src}
-							alt={alt}
-							width={45}
-							height={45}
-							className={styles.socials}
-						/>
-					</a>
-				))}
-			</div>
+			
+				<div className={clsx(styles.left_footer, styles.hack_icon)}>
+					{LEFT_SOCIALS.map(({ icon, link, alt }) => (
+						<a key={link} href={link} target="_blank">
+							<Image
+								src={icon.src}
+								alt={alt}
+								width={55}
+								height={55}
+								className={styles.socials}
+							/>
+						</a>
+					))}
+					</div>
+					<div className={styles.buttons}>
+					<div id={styles.box}>
+						<div className={clsx(styles.left_footer, styles.links)}>
+							<Link
+								href="https://zothacks-2023.devpost.com/"
+								className={styles.left_footer_text}
+								target="_blank"
+							>
+								Home
+							</Link>
+							<Link
+								href="https://zothacks-2023.devpost.com/"
+								className={styles.left_footer_text}
+								target="_blank"
+							>
+								Devpost
+							</Link>
+							<Link
+								href="https://zothacks-2023.devpost.com/"
+								className={styles.left_footer_text}
+								target="_blank"
+							>
+								About Us
+							</Link>
+						</div>
+						<div className={styles.right_footer}>
+							{RIGHT_SOCIALS.map(({ icon, link, alt }) => (
+								<a key={link} href={link} target="_blank">
+									<Image
+										src={icon.src}
+										alt={alt}
+										width={40}
+										height={40}
+										className={styles.socials}
+									/>
+								</a>
+							))}
+						</div>
+					</div>
+				
+				<a href="#" className={styles.button}>Apply Now</a>
+				</div>
+			<p className={styles.text}>Made with 👋 in Irvine, CA - Hack at UCI</p>
 		</footer>
 	);
 }
