95f9a8135b8cf7131f179119a7970f9031213260
IanWearsHat <ianbbdai@gmail.com>
2024-10-01 14:43:21 -0700

    Anteaters float with scroll

--- apps/site/src/app/(home)/sections/FAQ/AnteaterFloatie.tsx
+++ apps/site/src/app/(home)/sections/FAQ/AnteaterFloatie.tsx
@@ -3,19 +3,27 @@
 import Image from "next/image";
 import { ImageProps } from "next/image";
 
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 import styles from "./FAQ.module.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AnteaterFloatie = ({ src, alt, className }: ImageProps) => {
-	const { scrollYProgress } = useScroll();
+	const ref = useRef(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "end end"],
+	});
+	const y = useSpring(scrollYProgress, { bounce: 0.2 + Math.random() * 0.4 });
+	const yScaled = useTransform(y, (value) => value * 250);
 
 	const [isClicked, setIsClicked] = useState(false);
 
 	return (
-		<div>
+		<div ref={ref}>
 			<motion.div
+				style={{ y: yScaled }}
 				animate={{ rotate: [3.5, -3.5, 3.5], scale: isClicked ? 0.85 : 1 }}
 				transition={{
 					rotate: {
@@ -36,3 +44,25 @@ const AnteaterFloatie = ({ src, alt, className }: ImageProps) => {
 };
 
 export default AnteaterFloatie;
+/*
+
+class Circle extends Shape {
+	static final double PI = 3.14;
+	double radius;
+	
+	Circle(double newRadius) {
+		radius = newRadius;
+	}
+
+	public double area() {
+		return PI * radius * radius;
+	}
+}
+
+Shape[] shapes = new Shape[] {new Circle(10), new Circle(20)}
+
+for (Shape shape: shapes) {
+	System.out.println(shape.area());
+}
+
+*/
