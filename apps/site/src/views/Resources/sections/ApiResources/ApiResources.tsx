30cbefdc097f9806412a1e717463d1a7e4175067
IanWearsHat <ianbbdai@gmail.com>
2024-10-02 16:43:53 -0700

    Created Heading Card

--- apps/site/src/views/Resources/sections/ApiResources/ApiResources.tsx
+++ apps/site/src/views/Resources/sections/ApiResources/ApiResources.tsx
@@ -7,21 +7,21 @@ import ResourceCard from "../../components/ResourceCard/ResourceCard";
 import { getResources } from "../../getResources";
 import { client } from "@/lib/sanity/client";
 
+import HeadingCard from "../../components/HeadingCard/HeadingCard";
+
 async function ApiResources() {
 	const resources = await getResources("api");
 	return (
 		<div className="container">
-			{/* Card Component */}
-			<div className={styles.card}>
-				<h2 className={styles.title}>API Resources</h2>
-				<p className={styles.text}>
-					Application Programming Interface (API) are interfaces or
+			<HeadingCard
+				title="API Resources"
+				description="Application Programming Interface (API) are interfaces or
 					communication protocol that simplifies implementation and maintenance
-					of software. In order to access most API&apos;s, many languages use
+					of software. In order to access most API's, many languages use
 					HTTP protocol to communicate with the servers that host the API and
-					retrieve data.
-				</p>
-			</div>
+					retrieve data."
+			/>
+
 			<div className={styles["bottom-spacer"] + " row"}>
 				{/* Sticky Notes */}
 				{resources.map(
