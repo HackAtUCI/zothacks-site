29e3ead9a98e825fdbddb368eabd57c6b2c034f1
Jennifer <jennil38@uci.edu>
2024-10-03 22:52:45 -0700

    changed icons to official, commented out devpost button and added links to other buttons

--- apps/site/src/components/Footer/Footer.tsx
+++ apps/site/src/components/Footer/Footer.tsx
@@ -6,12 +6,11 @@ import clsx from "clsx";
 
 import styles from "./Footer.module.scss";
 
-
 import Mail from "@/assets/icons/mail_icon.svg";
-import Facebook from "@/assets/icons/facebook_icon.svg";
-import Instagram from "@/assets/icons/instagram_icon.svg";
+import Facebook from "@/assets/icons/facebook.svg";
+import Instagram from "@/assets/icons/Instagram_Glyph_White.svg";
 import Hack from "@/assets/icons/HACK LOGO.svg";
-import TikTok from "@/assets/icons/tiktok_icon.svg";
+import TikTok from "@/assets/icons/tiktok.svg";
 
 type Social = {
 	icon: any;
@@ -24,7 +23,7 @@ const LEFT_SOCIALS: Social[] = [
 		icon: Hack,
 		link: "https://hack.ics.uci.edu/",
 		alt: "Hack at UCI logo that links to Hack at UCI's club website",
-	}
+	},
 ];
 
 const RIGHT_SOCIALS: Social[] = [
@@ -53,62 +52,64 @@ const RIGHT_SOCIALS: Social[] = [
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			
-				<div className={clsx(styles.left_footer, styles.hack_icon)}>
-					{LEFT_SOCIALS.map(({ icon, link, alt }) => (
-						<a key={link} href={link} target="_blank">
-							<Image
-								src={icon.src}
-								alt={alt}
-								width={55}
-								height={55}
-								className={styles.socials}
-							/>
-						</a>
-					))}
+			<div className={clsx(styles.left_footer, styles.hack_icon)}>
+				{LEFT_SOCIALS.map(({ icon, link, alt }) => (
+					<a key={link} href={link} target="_blank">
+						<Image
+							src={icon.src}
+							alt={alt}
+							width={55}
+							height={55}
+							className={styles.socials}
+						/>
+					</a>
+				))}
+			</div>
+			<div className={styles.buttons}>
+				<div id={styles.box}>
+					<div className={clsx(styles.left_footer, styles.links)}>
+						<Link
+							href="/#"
+							className={styles.left_footer_text}
+						>
+							Home
+						</Link>
+						{/* <Link
+							href="https://zothacks-2023.devpost.com/"
+							className={styles.left_footer_text}
+							target="_blank"
+						>
+							Devpost
+						</Link> */}
+						<Link
+							href="https://zothacks-2023.devpost.com/"
+							className={styles.left_footer_text}
+							target="_blank"
+						>
+							About Us
+						</Link>
 					</div>
-					<div className={styles.buttons}>
-					<div id={styles.box}>
-						<div className={clsx(styles.left_footer, styles.links)}>
-							<Link
-								href="https://zothacks-2023.devpost.com/"
-								className={styles.left_footer_text}
-								target="_blank"
-							>
-								Home
-							</Link>
-							<Link
-								href="https://zothacks-2023.devpost.com/"
-								className={styles.left_footer_text}
-								target="_blank"
-							>
-								Devpost
-							</Link>
-							<Link
-								href="https://zothacks-2023.devpost.com/"
-								className={styles.left_footer_text}
-								target="_blank"
-							>
-								About Us
-							</Link>
-						</div>
-						<div className={styles.right_footer}>
-							{RIGHT_SOCIALS.map(({ icon, link, alt }) => (
-								<a key={link} href={link} target="_blank">
-									<Image
-										src={icon.src}
-										alt={alt}
-										width={40}
-										height={40}
-										className={styles.socials}
-									/>
-								</a>
-							))}
-						</div>
+					<div className={styles.right_footer}>
+						{RIGHT_SOCIALS.map(({ icon, link, alt }) => (
+							<a key={link} href={link} target="_blank">
+								<Image
+									src={icon.src}
+									alt={alt}
+									width={40}
+									height={40}
+									className={styles.socials}
+								/>
+							</a>
+						))}
 					</div>
-				
-				<a href="#" className={styles.button}>Apply Now</a>
 				</div>
+
+				<a href="https://docs.google.com/forms/d/e/1FAIpQLSeZN1njLZC9N_JE9eMYbtSZ95O1SafaZVLddTNReln6eZR7Vg/viewform" 
+				className={styles.button}
+				target="_blank">
+					Apply Now
+				</a>
+			</div>
 			<p className={styles.text}>Made with 👋 in Irvine, CA - Hack at UCI</p>
 		</footer>
 	);
