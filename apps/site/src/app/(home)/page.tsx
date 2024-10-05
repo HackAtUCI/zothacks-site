9130f1d371254da6b313a7205772ac4017419547
Jennifer <jennil38@uci.edu>
2024-10-05 15:05:42 -0700

    deleted unused icons, cleaned up code, removed unused components and imports

--- apps/site/src/app/(home)/page.tsx
+++ apps/site/src/app/(home)/page.tsx
@@ -1,11 +1,9 @@
 export const revalidate = 60;
 
 import Landing from "./sections/Landing";
-import Intro from "./sections/Intro";
 import GetInvolved from "./sections/GetInvolved";
 import Sponsors from "./sections/Sponsors";
 import FAQ from "./sections/FAQ";
-import Footer from "../../components/Footer/Footer";
 
 import styles from "./page.module.scss";
 
@@ -13,10 +11,6 @@ const Home = () => {
 	return (
 		<div className={styles.home}>
 			<Landing />
-			{/* <Intro />
-			<Mentor />
-			<Sponsors />
-			<FAQ /> */}
 			<GetInvolved />
 			{/* <Sponsors /> */}
 			{/* <FAQ /> */}
