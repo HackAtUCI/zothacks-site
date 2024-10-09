9061c158bc7869881e21d14c7c304af68348f4ef
Albert Wang <alberw5@uci.edu>
2024-10-08 20:35:56 -0700

    Fix background gradient and About Us description (#176)
    * update about us description
    
    * fix background gradient
    
    * fix about us text placement on different screen sizes

--- apps/site/src/app/(home)/sections/Intro/Intro.tsx
+++ apps/site/src/app/(home)/sections/Intro/Intro.tsx
@@ -5,11 +5,13 @@ export default function Intro() {
 		<div className={styles.intro}>
 			<h2 className={styles.header}>What is ZotHacks?</h2>
 			<p className={styles.description}>
-				ZotHacks is UC Irvine’s beginner-friendly hackathon, where students with
-				minimal computer science experience will learn to build their first CS
-				project. Through ZotHacks, we introduce these students to the world of
-				hackathons and web development by providing technical workshops, strong
-				mentorship, and free food!
+				ZotHacks is a 12-hour hackathon designed for beginners where students
+				with minimal computer science experience will learn to build their first
+				CS project. Through ZotHacks, we introduce these students to the world
+				of hackathons and web development by providing technical workshops,
+				dedicated mentors for every team, and free food! We encourage applicants
+				from all backgrounds, including underrepresented minorities, majors, or
+				genders to apply!
 			</p>
 		</div>
 	);
