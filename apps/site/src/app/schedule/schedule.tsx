9210f43f377c794883860284406273f9c3793ff3
Albert Wang <alberw5@uci.edu>
2024-10-13 19:36:11 -0700

    restore schedule page feature flag

---
+++ apps/site/src/app/schedule/schedule.tsx
@@ -0,0 +1,23 @@
+import { PortableText } from "@portabletext/react";
+import ClipboardSchedule from "./ClipboardSchedule";
+import { getSchedule } from "./getSchedule";
+
+import styles from "./page.module.scss";
+
+export default async function Schedule() {
+	const days = await getSchedule();
+
+	const schedule = days.map((events) =>
+		events.map(({ description, ...event }) => ({
+			...event,
+			description: <PortableText value={description} />,
+		})),
+	);
+
+	return (
+		<div className={styles.schedule}>
+			<h1>Schedule</h1>
+			<ClipboardSchedule schedule={schedule} />
+		</div>
+	);
+}
