1e45602303ca022950e346b834a15b082c4470ca
IanWearsHat <ianbbdai@gmail.com>
2024-10-21 13:32:09 -0700

    Replaced all section heading cards in resources

--- apps/site/src/views/Resources/sections/FrontendResources/FrontendResources.tsx
+++ apps/site/src/views/Resources/sections/FrontendResources/FrontendResources.tsx
@@ -6,19 +6,18 @@ import { getResources } from "../../getResources";
 import { client } from "@/lib/sanity/client";
 
 import styles from "./FrontendResources.module.scss";
+import HeadingCard from "../../components/HeadingCard/HeadingCard";
 
 async function FrontendResources() {
 	const resources = await getResources("frontend");
 	return (
 		<div className="container">
-			<div className={styles.card}>
-				<h2 className={styles.title}>Frontend Framework Resources</h2>
-				<p className={styles.text}>
-					Frontend frameworks aid in the design of user interfaces and
+			<HeadingCard
+				title="Frontend Framework Resources"
+				description="Frontend frameworks aid in the design of user interfaces and
 					experiences. Your users will interact with your services through the
-					frontend.
-				</p>
-			</div>
+					frontend."
+			/>
 			<div className={styles["bottom-spacer"] + " row"}>
 				{resources.map(
 					({ _id, title, description, link, logo, background }) => (
@@ -27,7 +26,9 @@ async function FrontendResources() {
 								title={title}
 								description={<PortableText value={description} />}
 								stickerSrc={urlImageBuilder(client).image(logo).url()}
-								islandBackground={urlImageBuilder(client).image(background).url()}
+								islandBackground={urlImageBuilder(client)
+									.image(background)
+									.url()}
 								links={[{ text: "Reference", link: link }]}
 							/>
 						</div>
--- apps/site/src/views/Resources/sections/StarterPacks/StarterPacks.tsx
+++ apps/site/src/views/Resources/sections/StarterPacks/StarterPacks.tsx
@@ -6,18 +6,17 @@ import { getResources } from "../../getResources";
 import { client } from "@/lib/sanity/client";
 
 import styles from "./StarterPacks.module.scss";
+import HeadingCard from "../../components/HeadingCard/HeadingCard";
 
 async function StarterPacks() {
 	const resources = await getResources("starter-pack");
 	return (
 		<div className="container">
-			<div className={styles.card}>
-				<h2 className={styles.title}>Starter Pack Resources</h2>
-				<p className={styles.text}>
-					Various starter packs to start you off on your coding journeys at
-					ZotHacks, featuring a variety of technologies and tech stacks!
-				</p>
-			</div>
+			<HeadingCard
+				title="Starter Pack Resources"
+				description="Various starter packs to start you off on your coding journeys at
+					ZotHacks, featuring a variety of technologies and tech stacks!"
+			/>
 			<div className={styles["bottom-spacer"] + " row"}>
 				{resources.map(
 					({ _id, title, description, link, logo, background }) => (
@@ -26,7 +25,9 @@ async function StarterPacks() {
 								title={title}
 								description={<PortableText value={description} />}
 								stickerSrc={urlImageBuilder(client).image(logo).url()}
-								islandBackground={urlImageBuilder(client).image(background).url()}
+								islandBackground={urlImageBuilder(client)
+									.image(background)
+									.url()}
 								links={[{ text: "Reference", link: link }]}
 							/>
 						</div>
