81cd98ebfcce08ca23f352851ea6a1a4571cc6ae
Noah Kim <noahmk5002@gmail.com>
2024-10-29 17:37:29 -0700

    Prettier

--- apps/site/src/app/(home)/sections/Landing/Characters.tsx
+++ apps/site/src/app/(home)/sections/Landing/Characters.tsx
@@ -10,13 +10,29 @@ import mainCharacter from "@/assets/images/intro-main-anteater.svg";
 import styles from "./Characters.module.scss";
 
 export default function Characters() {
-    return (
-        <div>
-            <Image src={waterAnteater} alt="Anteater floating in the water" className={styles.character} />
-            <Image src={waterAnteaterShadow} alt="Reflection of anteater in the water" className={styles.characterShadow} />
-            <Image src={waterAnteaterFoam} alt="Foam between anteater and the water" className={styles.characterFoam} />
-            <Image src={mainCharacter} alt="Anteater standing on the shore" className={styles.mainCharacter} />
-            <Image src={beachBall} alt="Beach ball" className={styles.beachBall} />
-        </div>
-    )
+	return (
+		<div>
+			<Image
+				src={waterAnteater}
+				alt="Anteater floating in the water"
+				className={styles.character}
+			/>
+			<Image
+				src={waterAnteaterShadow}
+				alt="Reflection of anteater in the water"
+				className={styles.characterShadow}
+			/>
+			<Image
+				src={waterAnteaterFoam}
+				alt="Foam between anteater and the water"
+				className={styles.characterFoam}
+			/>
+			<Image
+				src={mainCharacter}
+				alt="Anteater standing on the shore"
+				className={styles.mainCharacter}
+			/>
+			<Image src={beachBall} alt="Beach ball" className={styles.beachBall} />
+		</div>
+	);
 }
