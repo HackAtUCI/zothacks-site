47f2d68bea79ebfa0b2ac69b5eaa04208f9adf00
Noah Kim <noahmk5002@gmail.com>
2024-10-31 14:22:35 -0700

    Add landing page animations

--- apps/site/src/app/(home)/sections/Landing/Characters.tsx
+++ apps/site/src/app/(home)/sections/Landing/Characters.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import waterAnteater from "@/assets/images/intro-water-anteater.svg";
 import waterAnteaterFoam from "@/assets/images/intro-character-foam.svg";
@@ -12,27 +13,59 @@ import styles from "./Characters.module.scss";
 export default function Characters() {
 	return (
 		<div>
-			<Image
-				src={waterAnteater}
-				alt="Anteater floating in the water"
-				className={styles.character}
-			/>
-			<Image
-				src={waterAnteaterShadow}
-				alt="Reflection of anteater in the water"
-				className={styles.characterShadow}
-			/>
-			<Image
-				src={waterAnteaterFoam}
-				alt="Foam between anteater and the water"
-				className={styles.characterFoam}
-			/>
-			<Image
-				src={mainCharacter}
-				alt="Anteater standing on the shore"
-				className={styles.mainCharacter}
-			/>
-			<Image src={beachBall} alt="Beach ball" className={styles.beachBall} />
+			<motion.div
+				initial={{ translateX: 100, opacity: 0 }}
+				animate={{ translateX: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.6 }}
+			>
+				<Image
+					src={waterAnteater}
+					alt="Anteater floating in the water"
+					className={styles.character}
+				/>
+			</motion.div>
+			<motion.div
+				initial={{ translateX: 100, opacity: 0 }}
+				animate={{ translateX: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.6 }}
+			>
+				<Image
+					src={waterAnteaterShadow}
+					alt="Reflection of anteater in the water"
+					className={styles.characterShadow}
+				/>
+			</motion.div>
+
+			<motion.div
+				initial={{ translateX: 100, opacity: 0 }}
+				animate={{ translateX: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.6 }}
+			>
+				<Image
+					src={waterAnteaterFoam}
+					alt="Foam between anteater and the water"
+					className={styles.characterFoam}
+				/>
+			</motion.div>
+
+			<motion.div
+				initial={{ translateY: -100, opacity: 0 }}
+				animate={{ translateY: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 1 }}
+			>
+				<Image
+					src={mainCharacter}
+					alt="Anteater standing on the shore"
+					className={styles.mainCharacter}
+				/>
+			</motion.div>
+			<motion.div
+				initial={{ scale: 0.85, opacity: 0 }}
+				animate={{ scale: 1, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.8 }}
+			>
+				<Image src={beachBall} alt="Beach ball" className={styles.beachBall} />
+			</motion.div>
 		</div>
 	);
 }
