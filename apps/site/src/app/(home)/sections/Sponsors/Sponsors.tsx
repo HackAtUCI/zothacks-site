23446a7fd7c15a6f23c372cc20deb3857e16661f
Andrew Hwang <pandy430@gmail.com>
2024-10-31 19:06:37 -0700

    Merge main into feature/sponsors branch

--- apps/site/src/app/(home)/sections/Sponsors/Sponsors.tsx
+++ apps/site/src/app/(home)/sections/Sponsors/Sponsors.tsx
@@ -10,47 +10,22 @@ const builder = imageUrlBuilder(client);
 const Sponsors = async () => {
 	const sponsors = await getSponsors();
 
-	// placeholder sponsors (replace this section)
-	const fakeSponsorNames = [
-		"Ryan Yang",
-		"Ryan Yang",
-		"Ryan Yang",
-		"Ryan Yang",
-		"Ryan Yang",
-	];
-	const fakeSponsorLogos = fakeSponsorNames.map((name, index) => ({
-		_key: `fake-sponsor-${index}`,
-		name,
-		url: "#",
-		logo: {
-			asset: {
-				_ref: "image-placeholder",
-			},
-		},
-	}));
-
-	const allSponsors = [...sponsors.sponsors, ...fakeSponsorLogos];
-
 	return (
 		<Container as="section">
 			<h2 className={styles.title}>SPONSORS</h2>
 			<div className={styles.logos}>
-				{allSponsors.map(({ _key, name, url, logo }) => (
+				{sponsors.sponsors.map(({ _key, name, url, logo, tier }) => (
 					<a
 						key={_key}
 						href={url}
 						target="_blank"
 						rel="noopener noreferrer"
-						className={styles.logo}
+						className={`${styles.logo} ${styles[tier]}`}
 					>
-						{logo.asset._ref === "image-placeholder" ? (
-							<div className={styles.placeholderLogo}>{name}</div>
-						) : (
-							<img
-								src={builder.image(logo).format("webp").url()}
-								alt={name + " logo"}
-							/>
-						)}
+						<img
+							src={builder.image(logo).format("webp").url()}
+							alt={name + " logo"}
+						/>
 					</a>
 				))}
 			</div>
