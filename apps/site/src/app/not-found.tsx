fef9fb71eafa99f1fb8db7e2067ecc03f582ff2e
jotalis <jayqwu@gmail.com>
2024-11-07 04:07:25 -0800

    chore: resolve prettier formatting

--- apps/site/src/app/not-found.tsx
+++ apps/site/src/app/not-found.tsx
@@ -1,13 +1,13 @@
-import styles from './not-found.module.scss';
+import styles from "./not-found.module.scss";
 
 export default function NotFoundPage() {
 	return (
-		<div
-			className={styles['not-found-page']}
-		>
+		<div className={styles["not-found-page"]}>
 			<div className={styles.content}>
 				<h1 className={styles.heading}>404</h1>
-				<p className={styles.subtext}>Gone fishing! Seems this page got away…</p>
+				<p className={styles.subtext}>
+					Gone fishing! Seems this page got away…
+				</p>
 			</div>
 		</div>
 	);
