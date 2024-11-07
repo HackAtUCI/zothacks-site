2a527151ec0f8c6e7ce94a88b75b142cd514435f
jotalis <jayqwu@gmail.com>
2024-11-07 04:00:49 -0800

    feat: customize 404 not found page with background and message

---
+++ apps/site/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import styles from './not-found.module.scss';
+
+export default function NotFoundPage() {
+	return (
+		<div
+			className={styles['not-found-page']}
+		>
+			<div className={styles.content}>
+				<h1 className={styles.heading}>404</h1>
+				<p className={styles.subtext}>Gone fishing! Seems this page got away…</p>
+			</div>
+		</div>
+	);
+}
