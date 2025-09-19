47a9b9c4125ac5a0e4366b14cd20b469a6ba8286
Yousef <theyousefkhan@gmail.com>
2025-09-19 03:31:13 -0700

    Initial middleware scaffolding

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -2,24 +2,33 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	sassOptions: {
-		includePaths: [path.join(__dirname, "src", "lib", "styles")],
-	},
-	async redirects() {
-		return [
-			{
-				source: "/apply",
-				destination: "/",
-				permanent: true,
-			},
-			{
-				source: "/mentor",
-				destination: "/",
-				permanent: true,
-			},
-		];
-	},
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "src", "lib", "styles")],
+    },
+    async redirects() {
+        return [
+            {
+                source: "/apply",
+                destination: "/",
+                permanent: true,
+            },
+            {
+                source: "/mentor",
+                destination: "/",
+                permanent: true,
+            },
+        ];
+    },
+
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${process.env.IH_BACKEND_URL || "https://irvinehacks.com"}/api/:path*`,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
