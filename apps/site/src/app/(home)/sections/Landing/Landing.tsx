358bdd3b0f9d158fee24a617ad7cb552b89eefa9
Ethan Chin <ejchin3@uci.edu>
2025-09-21 16:42:49 -0700

    reverted back to old Title and ApplyButton and changed from <p> to <h2>

--- apps/site/src/app/(home)/sections/Landing/Landing.tsx
+++ apps/site/src/app/(home)/sections/Landing/Landing.tsx
@@ -5,7 +5,7 @@ const Landing = () => {
 	return (
 		<div className={styles.landing}>
 			<h1 className={styles.title}>ZotHacks 2025</h1>
-			<p className={styles.subtitle}>Theme Coming Soon</p>
+			<h2 className={styles.subtitle}>Theme Coming Soon</h2>
 			<a href="/apply-mentor" className={styles.applyButton}>
 				Mentor Applications Now Open
 			</a>
