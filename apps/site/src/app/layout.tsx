afa73fa963cfd337e24014131f7ab091e4547063
Ethan Chin <ejchin3@uci.edu>
2025-09-21 00:26:33 -0700

    kinda working coming soon but tablet looks terrible

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Fredoka } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
@@ -7,19 +7,21 @@ import "@/lib/styles/bootstrap.scss";
 import "@/lib/styles/globals.scss";
 
 export const metadata: Metadata = {
-	title: "ZotHacks 2024",
+	title: "ZotHacks 2025",
 	description: "Hack at UCI's premier hackathon for beginners at UCI",
 	openGraph: {
-		title: "ZotHacks 2024",
+		title: "ZotHacks 2025",
 		images: "./zothacks_banner.png",
 		description: "Hack at UCI's premier hackathon for beginners at UCI",
 	},
 };
 
-const fredoka = Fredoka({
+
+const dmSans = DM_Sans({
 	weight: ["400", "500", "600", "700"],
 	subsets: ["latin"],
-	variable: "--next-font-fredoka",
+	display: "swap",
+	variable: "--font-dm-sans", 
 });
 
 export default function RootLayout({
@@ -28,7 +30,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" className={fredoka.variable}>
+		<html lang="en" className={`${dmSans.variable}`}>
 			<body className="background">
 				<NavBar />
 				<main>{children}</main>
