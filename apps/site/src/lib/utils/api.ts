6ce351d347f2c57e0ec59d5db9e7ba5520fa3aad
ianbb <ianbbdai@gmail.com>
2025-09-22 02:20:12 -0700

    Updated api.ts to use IH api

--- apps/site/src/lib/utils/api.ts
+++ apps/site/src/lib/utils/api.ts
@@ -1,8 +1,25 @@
 import axios from "axios";
+import { cookies } from "next/headers";
 
+const LOCAL_API_URL = "http://localhost:8000";
+const SERVER_HOST = process.env.VERCEL_URL;
+
+// The Vercel Serverless Function for the API lives outside the scope of Next.js
+// so the publicly deployed URL must be used instead of a rewrite
 const api = axios.create({
-  baseURL: process.env.IH_API_BASE, 
-  withCredentials: true,                
+	baseURL: SERVER_HOST ? `https://${SERVER_HOST}/api/` : LOCAL_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+	const cookieStore = cookies();
+
+	// Inject user's client-side cookies along with API request
+	const provided = config.headers.get("Cookie");
+	const newCookies = (provided ? `${provided}; ` : "") + cookieStore.toString();
+	config.headers.set("Cookie", newCookies);
+	config.headers.set("X-Hackathon-Name", "irvinehacks");
+
+	return config;
 });
 
 export default api;
