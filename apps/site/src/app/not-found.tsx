21678c1cf6923504b5dd63889b5f9a3041939724
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-09-25 15:16:20 -0700

    Updated 404 page to remove the old zothacks image and fishing text.

--- apps/site/src/app/not-found.tsx
+++ apps/site/src/app/not-found.tsx
@@ -1,14 +1,31 @@
+import { Button } from "react-bootstrap";
 import styles from "./not-found.module.scss";
 
-export default function NotFoundPage() {
-	return (
-		<div className={styles["not-found-page"]}>
-			<div className={styles.content}>
-				<h1 className={styles.heading}>404</h1>
-				<p className={styles.subtext}>
-					Gone fishing! Seems this page got away…
-				</p>
-			</div>
+// export default function NotFoundPage() {
+// 	return (
+// 		<div className={styles["not-found-page"]}>
+// 			<div className={styles.content}>
+// 				<h1 className={styles.heading}>404</h1>
+// 				<p className={styles.subtext}>
+// 					Gone fishing! Seems this page got away…
+// 				</p>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className={styles["not-found-page"]}>
+		<div className={styles.content}>
+			<h1 className={styles.heading}>404</h1>
+			<p className={styles.subtext}>
+				Could not find requested resource
+ 			</p>
+			<Button className={styles.button} href="/">Return Home</Button>
 		</div>
-	);
+    </div>
+  )
 }
