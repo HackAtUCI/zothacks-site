daffefabfa134644ec3693d4fc00d360d2a42d27
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-09-28 10:59:38 -0700

    Ran prettier on changed files

--- apps/site/src/app/not-found.tsx
+++ apps/site/src/app/not-found.tsx
@@ -1,31 +1,17 @@
 import { Button } from "react-bootstrap";
 import styles from "./not-found.module.scss";
 
-// export default function NotFoundPage() {
-// 	return (
-// 		<div className={styles["not-found-page"]}>
-// 			<div className={styles.content}>
-// 				<h1 className={styles.heading}>404</h1>
-// 				<p className={styles.subtext}>
-// 					Gone fishing! Seems this page got away…
-// 				</p>
-// 			</div>
-// 		</div>
-// 	);
-// }
 
-import Link from 'next/link'
- 
 export default function NotFound() {
-  return (
-    <div className={styles["not-found-page"]}>
-		<div className={styles.content}>
-			<h1 className={styles.heading}>404</h1>
-			<p className={styles.subtext}>
-				Could not find requested resource
- 			</p>
-			<Button className={styles.button} href="/">Return Home</Button>
+	return (
+		<div className={styles["not-found-page"]}>
+			<div className={styles.content}>
+				<h1 className={styles.heading}>404</h1>
+				<p className={styles.subtext}>Could not find requested resource</p>
+				<Button className={styles.button} href="/">
+					Return Home
+				</Button>
+			</div>
 		</div>
-    </div>
-  )
+	);
 }
