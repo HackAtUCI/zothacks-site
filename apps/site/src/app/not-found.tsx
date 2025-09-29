acf0d5ada4f17376a26906a7aab47616563a9e37
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-09-28 22:45:13 -0700

    Fixed typescript button issue

--- apps/site/src/app/not-found.tsx
+++ apps/site/src/app/not-found.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { Button } from "react-bootstrap";
 import styles from "./not-found.module.scss";
+import { useRouter } from "next/navigation";
+
 
 export default function NotFound() {
+	const router = useRouter();
 	return (
 		<div className={styles["not-found-page"]}>
 			<div className={styles.content}>
 				<h1 className={styles.heading}>404</h1>
 				<p className={styles.subtext}>Could not find requested resource</p>
-				<Button className={styles.button} href="/">
+				<button type="button" className={styles.button} onClick={() => router.push("/")}>
 					Return Home
-				</Button>
+				</button>
 			</div>
 		</div>
 	);
