fa49f224515107c6ec34cd4018465de335977659
ianbb <ianbbdai@gmail.com>
2025-09-28 17:35:29 -0700

    Prettier

--- apps/site/src/app/not-found.tsx
+++ apps/site/src/app/not-found.tsx
@@ -1,7 +1,6 @@
 import { Button } from "react-bootstrap";
 import styles from "./not-found.module.scss";
 
-
 export default function NotFound() {
 	return (
 		<div className={styles["not-found-page"]}>
