e356efb1dd6bfbf1d9b1a2e02b436880067a8506
Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
2025-09-28 21:37:45 -0700

    Implemented confirmation page (#246)
    * initial commit for confirmation page
    
    * removed hacker and mentor anteaters from assets
    
    * moved confirmation page to /portal on submit
    
    * ran prettier
    
    * minor cleanups and changed button hover effect
    
    * ran prettier

---
+++ apps/site/src/app/portal/ConfirmationPage.tsx
@@ -0,0 +1,16 @@
+import styles from "./confirmation.module.scss";
+import Image from "next/image";
+import mascots from "@/assets/images/mentor-hacker-anteaters.svg";
+
+export default function ConfirmationPage() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.messageBox}>
+				<Image src={mascots} alt="Hacker Anteater" className={styles.mascot} />
+				<h1 className={styles.title}>Thank you for applying!</h1>
+			</div>
+
+			<button className={styles.button}>Return to Homepage</button>
+		</div>
+	);
+}
