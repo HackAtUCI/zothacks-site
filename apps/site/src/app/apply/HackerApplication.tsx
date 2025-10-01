7677f41704da06f711d3835499c475b5c07099ef
ianbb <ianbbdai@gmail.com>
2025-10-01 00:00:01 -0700

    Added anteater image

--- apps/site/src/app/apply/HackerApplication.tsx
+++ apps/site/src/app/apply/HackerApplication.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
+
 import BaseForm from "@/components/BaseForm/BaseForm";
 import ApplicationForm from "./ApplicationForm";
 import EventInfoCard from "./EventInfoCard";
+import hackerAnteater from "@/assets/images/hacker-anteater.png";
+
 import styles from "./HackerApplication.module.scss";
 
 export default function HackerApplication() {
 	return (
 		<BaseForm applyPath="/api/user/apply" applicationType="Hacker">
 			<div className={styles.container}>
+				<Image src={hackerAnteater} alt="hacker anteater" />
 				<h1 className={styles.title}>ZotHacks 2025: Hacker Application</h1>
 				<EventInfoCard />
 				<ApplicationForm />
