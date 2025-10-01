507abe49683492203c1114d4b4833f6ade9d6158
ianbb <ianbbdai@gmail.com>
2025-10-01 00:14:26 -0700

    Added background

--- apps/site/src/app/apply/HackerApplication.tsx
+++ apps/site/src/app/apply/HackerApplication.tsx
@@ -4,18 +4,26 @@ import BaseForm from "@/components/BaseForm/BaseForm";
 import ApplicationForm from "./ApplicationForm";
 import EventInfoCard from "./EventInfoCard";
 import hackerAnteater from "@/assets/images/hacker-anteater.png";
+import background from "@/assets/images/application-background.png";
 
 import styles from "./HackerApplication.module.scss";
 
 export default function HackerApplication() {
 	return (
-		<BaseForm applyPath="/api/user/apply" applicationType="Hacker">
-			<div className={styles.container}>
-				<Image src={hackerAnteater} alt="hacker anteater" />
-				<h1 className={styles.title}>ZotHacks 2025: Hacker Application</h1>
-				<EventInfoCard />
-				<ApplicationForm />
-			</div>
-		</BaseForm>
+		<div className={styles.container}>
+			<BaseForm applyPath="/api/user/apply" applicationType="Hacker">
+				<Image
+					src={background}
+					alt="tree background"
+					className={styles.background}
+				/>
+				<div className={styles.formContainer}>
+					<Image src={hackerAnteater} alt="hacker anteater" />
+					<h1 className={styles.title}>ZotHacks 2025: Hacker Application</h1>
+					<EventInfoCard />
+					<ApplicationForm />
+				</div>
+			</BaseForm>
+		</div>
 	);
 }
