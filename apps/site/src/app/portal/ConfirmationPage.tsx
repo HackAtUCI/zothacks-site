19aa6f561ffff22a97371b7ff9e2b22f0ce2e013
ianbb <ianbbdai@gmail.com>
2025-10-02 15:34:51 -0700

    Added clarifying text to confirmation

--- apps/site/src/app/portal/ConfirmationPage.tsx
+++ apps/site/src/app/portal/ConfirmationPage.tsx
@@ -10,7 +10,14 @@ export default function ConfirmationPage() {
 		<div className={styles.container}>
 			<div className={styles.messageBox}>
 				<Image src={mascots} alt="Hacker Anteater" className={styles.mascot} />
-				<h1 className={styles.title}>Thank you for applying!</h1>
+				<h1 className={styles.title}>
+					Thank you for applying!
+					<br />
+					<br />
+					We have received your application
+					<br />
+					and you should hear back from us soon!
+				</h1>
 			</div>
 
 			<PrimaryButton href="/">Return to Homepage</PrimaryButton>
