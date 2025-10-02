b14c8c180bf6c97afda770cebebc6a7e5f16f476
ianbb <ianbbdai@gmail.com>
2025-10-01 23:52:26 -0700

    Added small variant to primary button

--- apps/site/src/components/PrimaryButton/PrimaryButton.tsx
+++ apps/site/src/components/PrimaryButton/PrimaryButton.tsx
@@ -3,16 +3,32 @@ import React from "react";
 import clsx from "clsx";
 
 import styles from "./PrimaryButton.module.scss";
+import Link from "next/link";
 
 interface PrimaryButtonProps {
 	href: string;
 	children: React.ReactNode;
 	className?: string;
+	variant?: "small" | "large";
 }
 
-const PrimaryButton = ({ href, children, className }: PrimaryButtonProps) => {
-	return (
-		<a href={href} className={clsx(styles.primaryButton, className)}>
+const PrimaryButton = ({
+	href,
+	children,
+	className,
+	variant = "large",
+}: PrimaryButtonProps) => {
+	const variants = {
+		small: styles.small,
+		large: styles.large,
+	};
+
+	return href.startsWith("/") ? (
+		<Link href={href} className={clsx(variants[variant], className)}>
+			{children}
+		</Link>
+	) : (
+		<a href={href} className={clsx(variants[variant], className)}>
 			{children}
 		</a>
 	);
