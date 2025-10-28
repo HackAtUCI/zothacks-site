6930e57075296f9667887824a4f608ea7a1d2496
ianbb <ianbbdai@gmail.com>
2025-10-28 00:18:33 -0700

    Removed banner

--- apps/site/src/app/(home)/sections/Landing/Landing.tsx
+++ apps/site/src/app/(home)/sections/Landing/Landing.tsx
@@ -23,27 +23,6 @@ const Landing = () => {
 				>
 					Apply Now
 				</PrimaryButton> */}
-				<div className={styles.errorBanner}>
-					<p>
-						<strong className={styles.important}>
-							IMPORTANT: If you applied as a hacker between <br />
-							Oct. 14, 11:59 p.m. and Oct. 17, 4:00 p.m.
-						</strong>{" "}
-						<br />
-						your application was not saved to our system due to a timing issue.
-					</p>
-					<ul>
-						<li>If you applied during that time, you will need to reapply.</li>
-						<li>
-							Hacker applications are NOT first-come, first-served, so applying
-							later won&apos;t hurt your chances at all.
-						</li>
-					</ul>
-					<p>
-						We truly apologize for the inconvenience and appreciate your
-						patience.
-					</p>
-				</div>
 			</section>
 		</div>
 	);
