8da3d4dd6f598e70837767bbb777ca543a916245
Nathan Ong <nathanong1217@gmail.com>
2026-05-18 14:40:01 -0700

    removed about retro window testing

--- apps/site/src/app/(home)/sections/About/About.tsx
+++ apps/site/src/app/(home)/sections/About/About.tsx
@@ -22,17 +22,16 @@ const About = () => {
 	return (
 		<section className={styles.about}>
 			<div className={styles.content}>
-				<RetroWindow title="What is ZotHacks?">
-					<p className={styles.aboutWindowText}>
-						ZotHacks is a 12-hour hackathon designed for beginners where
-						students with minimal computer science experience will learn to
-						build their first CS project. Through ZotHacks, we introduce these
-						students to the world of hackathons and web development by providing
-						technical workshops, dedicated mentors for every team, and free
-						food! We encourage applicants from all backgrounds, including
-						underrepresented minorities, majors, or genders to apply!
-					</p>
-				</RetroWindow>
+				<h3 className={styles.aboutTitle}>What is ZotHacks?</h3>
+				<p className={styles.aboutText}>
+					ZotHacks is a 12-hour hackathon designed for beginners where students
+					with minimal computer science experience will learn to build their
+					first CS project. Through ZotHacks, we introduce these students to the
+					world of hackathons and web development by providing technical
+					workshops, dedicated mentors for every team, and free food! We
+					encourage applicants from all backgrounds, including underrepresented
+					minorities, majors, or genders to apply!
+				</p>
 			</div>
 			{/* Characters + call-to-action text/buttons */}
 			<div className={styles.characters}>
