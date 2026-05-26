23b8af8307e99f2c6d7fba8da7a39156aee7cac0
Joshua Sullivan <joshee.sullivan@gmail.com>
2026-05-26 14:33:51 -0700

    Implement 404 Page (#332)
    * feat: added sleepy peter pic
    
    * feat: implemented main window + content
    
    * feat: added mobile responsiveness
    
    * chore: removed old fishing image
    
    ---------
    
    Co-authored-by: Nathan Ong <nathanong1217@gmail.com>
    Co-authored-by: Nathan Ong <164730140+NoNathan17@users.noreply.github.com>

--- apps/site/src/app/portal/ConfirmationPage.tsx
+++ apps/site/src/app/portal/ConfirmationPage.tsx
@@ -1,24 +1,58 @@
+"use client";
+
 import Image from "next/image";
 
 import mascots from "@/assets/images/mentor-hacker-anteaters.png";
 import PrimaryButton from "@/components/PrimaryButton/PrimaryButton";
+import { Status } from "@/lib/userRecord";
 
 import styles from "./Confirmation.module.scss";
 
-export default function ConfirmationPage() {
+type ConfirmationPageProps = {
+	status: string;
+};
+
+export default function ConfirmationPage({ status }: ConfirmationPageProps) {
+	const isAccepted = status === Status.Accepted;
+	const isWaiverSigned = status === Status.Signed;
+
 	return (
 		<div className={styles.container}>
 			<Image src={mascots} alt="Hacker Anteater" className={styles.mascot} />
 			<div className={styles.messageBox}>
 				<h1 className={styles.title}>
-					Thank you for applying!
-					<br />
-					<br />
-					We have received your application
-					<br />
-					and you should hear back from us soon!
+					{isAccepted ? (
+						<>
+							Congratulations!
+							<br />
+							<br />
+							You have been accepted to ZotHacks.
+							<br />
+							Please sign your waiver to continue.
+						</>
+					) : isWaiverSigned ? (
+						<>
+							Thank you!
+							<br />
+							<br />
+							Your waiver has been signed.
+						</>
+					) : (
+						<>
+							Thank you for applying!
+							<br />
+							<br />
+							We have received your application
+							<br />
+							and you should hear back from us soon!
+						</>
+					)}
 				</h1>
 			</div>
+
+			{isAccepted && (
+				<PrimaryButton href="/api/user/waiver">Sign Waiver</PrimaryButton>
+			)}
 
 			<PrimaryButton href="/">Return to Homepage</PrimaryButton>
 		</div>
